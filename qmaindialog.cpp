8a1c62d942d09653db98708bceaf2ab0c2e6fbbb
Boris Barbulovski <bbarbulovski@gmail.com>
2023-01-15 14:27:15 +0100

    Fix dir.entryList() error.

--- qmaindialog.cpp
+++ qmaindialog.cpp
@@ -75,7 +75,7 @@ void QMainDialog::on_lineEdit_DestPath_textChanged(const QString &arg1)
         ui->lineEdit_DestPath->setToolTip(tr("Dir do not exist."));
         ui->pushButton_ScanConvert->setEnabled(false);
     }
-    else if (dir.entryList().isEmpty())
+    else if (!dir.entryList().isEmpty())
     {
         ui->lineEdit_DestPath->setStyleSheet("background: red");
         ui->lineEdit_DestPath->setToolTip(tr("Dir is not empty."));
